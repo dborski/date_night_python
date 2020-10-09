8a89b56a925417da52164fd5ea12b782e34fe255
Derek Borski <derek.borski@gmail.com>
2020-10-08 22:01:27 -0600

    Add spec for head of tree being none by default

--- tests/test_binary_search_tree.py
+++ tests/test_binary_search_tree.py
@@ -3,7 +3,10 @@ from lib.binary_search_tree import BinarySearchTree
 
 class BinarySearchTreeTest(unittest.TestCase):
   def setUp(self):
-    self.search_tree = BinarySearchTree()
+    self.tree = BinarySearchTree()
   
   def test_it_exists(self):
-    self.assertIsInstance(self.search_tree, BinarySearchTree)
+    self.assertIsInstance(self.tree, BinarySearchTree)
+
+  def test_head_is_none_by_default(self):
+    self.assertEqual(self.tree.head, None)
