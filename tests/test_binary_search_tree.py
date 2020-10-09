c5fd3a27731659bfdd7653715950c811e80c656a
Derek Borski <derek.borski@gmail.com>
2020-10-08 22:55:21 -0600

    Add spec for insert function

--- tests/test_binary_search_tree.py
+++ tests/test_binary_search_tree.py
@@ -10,4 +10,9 @@ class BinarySearchTreeTest(unittest.TestCase):
 
   def test_head_is_none_by_default(self):
     self.assertEqual(self.tree.head, None)
-    
+
+  def test_insert(self):
+    self.assertEqual(self.tree.insert(61, "Bill & Ted's Excellent Adventure"), 0)
+    self.assertEqual(self.tree.insert(16, "Johnny English"), 1)
+    self.assertEqual(self.tree.insert(92, "Sharknado 3"), 1)
+    self.assertEqual(self.tree.insert(50, "Hannibal Buress: Animal Furnace"), 2)
