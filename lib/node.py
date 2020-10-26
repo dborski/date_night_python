5791f38353b2633eb04c50343f2652e1f51985f1
Derek Borski <derek.borski@gmail.com>
2020-10-26 15:52:07 -0600

    Add insert functions for both binary_search_tree and node classes. Also have insert_left and insert_right for node class that works recursively to insert the nodes in the search tree correctly

--- lib/node.py
+++ lib/node.py
@@ -2,6 +2,33 @@ class Node:
   def __init__(self, movie_score, movie_title):
     self.movie_score = movie_score
     self.movie_title = movie_title
-    self.next_left = None
-    self.next_right = None
+    self.left = None
+    self.right = None
+    self.depth = 0
+
+  def insert(self, movie_score, movie_title):
+    if self.movie_score > movie_score:
+      return self.insert_left(movie_score, movie_title)
+
+    elif self.movie_score < movie_score:
+      return self.insert_right(movie_score, movie_title)
+    else:
+      return "That movie score has already been used. Please submit another one"
     
+  def insert_left(self, movie_score, movie_title):
+      if self.left == None:
+        self.left = Node(movie_score, movie_title)
+        self.left.depth = self.depth + 1
+        return self.left.depth
+      else:
+        return self.left.insert(movie_score, movie_title)
+
+  def insert_right(self, movie_score, movie_title):
+      if self.right == None:
+        self.right = Node(movie_score, movie_title)
+        self.right.depth = self.depth + 1
+        return self.right.depth
+      else:
+        return self.right.insert(movie_score, movie_title)
+
+
