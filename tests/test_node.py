bad70347efc346cffab04acfca09168fde6ae546
Derek Borski <derek.borski@gmail.com>
2020-10-26 15:51:23 -0600

    Add assertions for insert_left and insert_right

--- tests/test_node.py
+++ tests/test_node.py
@@ -17,3 +17,26 @@ class NodeTest(unittest.TestCase):
 
   def test_right_is_none_by_default(self):
     self.assertEqual(self.node.right, None)
+
+  def test_insert_left(self):
+    self.node.insert(56, "Bill & Ted's Excellent Adventure")
+
+    self.assertEqual(self.node.left.movie_score, 56)
+
+    self.node.insert(50, "Die Hard")
+
+    self.assertEqual(self.node.left.left.movie_score, 50)
+
+    self.assertEqual(self.node.insert(50, "Jingle Bells"), "That movie score has already been used. Please submit another one")
+
+  def test_insert_right(self):
+    self.node.insert(64, "Bill & Ted's Excellent Adventure")
+
+    self.assertEqual(self.node.right.movie_score, 64)
+
+    self.node.insert(70, "Die Hard")
+
+    self.assertEqual(self.node.right.right.movie_score, 70)
+
+    self.assertEqual(self.node.insert(60, "Jingle Bells"), "That movie score has already been used. Please submit another one"
+    )
