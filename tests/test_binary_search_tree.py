56e5c933ac1629a8f425a145253a7407e8f87db0
Derek Borski <derek.borski@gmail.com>
2020-10-26 16:06:25 -0600

    Add tests for has_score function

--- tests/test_binary_search_tree.py
+++ tests/test_binary_search_tree.py
@@ -19,3 +19,19 @@ class BinarySearchTreeTest(unittest.TestCase):
     self.assertEqual(self.tree.insert(60, "Die Hard"), 3)
     self.assertEqual(self.tree.insert(14, "Hot Tub Time Machine"), 2)
     self.assertEqual(self.tree.insert(19, "Catwoman"), 3)
+
+  def test_has_score(self):
+    self.tree.insert(61, "Bill & Ted's Excellent Adventure"), 0
+    self.tree.insert(16, "Johnny English"), 1
+    self.tree.insert(92, "Sharknado 3"), 1
+    self.tree.insert(50, "Hannibal Buress: Animal Furnace"), 2
+    self.tree.insert(60, "Die Hard"), 3
+    self.tree.insert(14, "Hot Tub Time Machine"), 2
+    self.tree.insert(19, "Catwoman"), 3
+
+    self.assertEqual(self.tree.has_score(61), True)
+    self.assertEqual(self.tree.has_score(16), True)
+    self.assertEqual(self.tree.has_score(19), True)
+    self.assertEqual(self.tree.has_score(72), False)
+    self.assertEqual(self.tree.has_score(61), False)
+    self.assertEqual(self.tree.has_score(15), False)
