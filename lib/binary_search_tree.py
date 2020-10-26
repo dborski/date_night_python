5e4babdc05f78c0ab6a25280482910fbf7e99659
Derek Borski <derek.borski@gmail.com>
2020-10-26 16:46:12 -0600

    First working version of has_score function. Need to refactor

--- lib/binary_search_tree.py
+++ lib/binary_search_tree.py
@@ -10,3 +10,20 @@ class BinarySearchTree:
       return self.head.depth
     else:
       return self.head.insert(movie_score, movie_title)
+
+  def has_score(self, movie_score, current_node=None):
+    if current_node is None:
+      current_node = self.head
+
+    if current_node is None:
+      return False
+    else:
+      if current_node.movie_score == movie_score:
+        return True
+      elif current_node.depth > 0 and current_node.left == None and current_node.right == None:
+        return False
+      else:
+        if movie_score > current_node.movie_score:
+          return self.has_score(movie_score, current_node.right)
+        else:
+          return self.has_score(movie_score, current_node.left)
