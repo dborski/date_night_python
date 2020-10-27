e9d9ee443a8fe330bc66fc3f38ffc767f0e5a7fe
Derek Borski <derek.borski@gmail.com>
2020-10-26 18:04:14 -0600

    Add depth_of function. Also refactored since this is very similar to has_score function. Created new score_depth_helper function that contains logic for both functions

--- lib/binary_search_tree.py
+++ lib/binary_search_tree.py
@@ -12,15 +12,24 @@ class BinarySearchTree:
       return self.head.insert(movie_score, movie_title)
 
   def has_score(self, movie_score, current_node=None):
+    return self.score_depth_helper(movie_score, False, True, current_node)
+
+
+  def depth_of(self, movie_score, current_node=None):
+      return self.score_depth_helper(movie_score, None, False, current_node)
+      
+  def score_depth_helper(self, movie_score, first_return, second_return, current_node=None):
     if current_node is None:
       current_node = self.head
 
     if current_node is None:
-      return False
+      return first_return
     else:
       if current_node.movie_score == movie_score:
-        return True
+        return current_node.depth if second_return == False else second_return
       elif current_node.depth > 0 and current_node.left == None and current_node.right == None:
-        return False
+        return first_return
       else:
-        return self.has_score(movie_score, current_node.right if movie_score > current_node.movie_score else current_node.left)
+        return self.score_depth_helper(movie_score, first_return, second_return, current_node.right if movie_score > current_node.movie_score else current_node.left)
+
+
