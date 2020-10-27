70b4065bcfd6681a033a01e7004bce6357baccc3
Derek Borski <derek.borski@gmail.com>
2020-10-26 19:08:35 -0600

    Add first working version of max function that returns the title and score of the highest score in the list

--- lib/binary_search_tree.py
+++ lib/binary_search_tree.py
@@ -1,5 +1,10 @@
 from lib.node import Node
 
+def _movie_payload(node):
+  return {
+    node.movie_title: node.movie_score
+  }
+
 class BinarySearchTree:
   def __init__(self):
     self.head = None
@@ -14,9 +19,20 @@ class BinarySearchTree:
   def has_score(self, movie_score, current_node=None):
     return self.score_depth_helper(movie_score, False, True, current_node)
 
-
   def depth_of(self, movie_score, current_node=None):
       return self.score_depth_helper(movie_score, None, False, current_node)
+  
+  def max(self, current_node=None):
+    if current_node is None:
+      current_node = self.head
+
+    if current_node is None:
+      return None
+    else:
+      if current_node.right:
+        return self.max(current_node.right)
+      else:
+        return _movie_payload(current_node)
       
   def score_depth_helper(self, movie_score, first_return, second_return, current_node=None):
     if current_node is None:
